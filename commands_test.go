1e716965347c6ebdec1e555b118744c0c032df83
Daniel Mascarenhas <daniel.tavares@corp.globo.com>
2017-01-13 17:48:31 -0200

    replace, append and prepend tests

--- commands_test.go
+++ commands_test.go
@@ -52,7 +52,66 @@ func (suite *VShardCommandsTestSuite) TestAdd() {
 
 	value, err := suite.Pool.Get(key)
 	suite.NoError(err)
-	suite.Equal(expected, string(value))
+	suite.Equal(expectedValue, string(value))
+}
+
+func (suite *VShardCommandsTestSuite) TestReplaceInexistentKey() {
+	key := "replace-key-does-not-exist"
+	expectedValue := "vshard-test-replace"
+	ok, err := suite.Pool.Replace(key, 0, 0, []byte(expectedValue))
+	suite.False(ok)
+	suite.NoError(err)
+}
+
+func (suite *VShardCommandsTestSuite) TestReplace() {
+	key := "replace-key"
+	initialValue := "initial-value"
+	ok, err := suite.Pool.Set(key, 0, 0, []byte(initialValue))
+	suite.True(ok)
+	suite.NoError(err)
+
+	expectedValue := "replaced-value"
+	ok, err = suite.Pool.Replace(key, 0, 0, []byte(expectedValue))
+	suite.True(ok)
+	suite.NoError(err)
+
+	value, err := suite.Pool.Get(key)
+	suite.NoError(err)
+	suite.Equal(expectedValue, string(value))
+}
+
+func (suite *VShardCommandsTestSuite) TestAppend() {
+	key := "append-key"
+	initialValue := "initial-value"
+	ok, err := suite.Pool.Set(key, 0, 0, []byte(initialValue))
+	suite.True(ok)
+	suite.NoError(err)
+
+	appendValue := ":second-value"
+	ok, err = suite.Pool.Append(key, 0, 0, []byte(appendValue))
+	suite.True(ok)
+	suite.NoError(err)
+
+	value, err := suite.Pool.Get(key)
+	suite.NoError(err)
+	suite.Equal(initialValue+appendValue, string(value))
+}
+
+func (suite *VShardCommandsTestSuite) TestPrepend() {
+	key := "prepend-key"
+	initialValue := "initial-value"
+	ok, err := suite.Pool.Set(key, 0, 0, []byte(initialValue))
+	suite.True(ok)
+	suite.NoError(err)
+
+	prependValue := "second-value:"
+	ok, err = suite.Pool.Prepend(key, 0, 0, []byte(prependValue))
+	suite.True(ok)
+	suite.NoError(err)
+
+	value, err := suite.Pool.Get(key)
+	suite.NoError(err)
+	suite.Equal(prependValue+initialValue, string(value))
 }
 
 func TestVShardCommandsTestSuite(t *testing.T) {
